6f3435ec6de63045d756a71c16fa5b6acc69c478
Frank17008 <405267330@qq.com>
2020-05-19 20:19:53 +0800

    矢量建筑数据切片

--- patchConvert.js
+++ patchConvert.js
@@ -1,10 +1,11 @@
 const fs = require("fs");
-const dirGeojson = "D:/mapBoxGL/"; // geojson文件目录
-const dirPbf = "D:/PBF/"; // 生成切片的目录
+const dirGeojson = "D:/output/"; // geojson文件目录
+const dirPbf = "D:/mapBoxGL/PBF/"; // 生成切片的目录
 fs.readdir(dirGeojson, function (err, files) {
 	let res = "";
 	files.forEach((item) => {
-		res += "tippecanoe -e " + dirPbf + item.split(".")[0] + " -pC -Z12 -z18 -f " + dirGeojson + item + ";";
+		// res += "tippecanoe -e " + dirPbf + item.split(".")[0] + " -pC -Z10 -z18 -f " + dirGeojson + item + ";";
+		res += "tippecanoe -e " + dirPbf + " -pC -Z10 -z18 -f " + dirGeojson + item + ";";
 	});
 	// 将生成切片命令写入文件
 	fs.writeFile("./tippecanoe-command.txt", res, function () {
